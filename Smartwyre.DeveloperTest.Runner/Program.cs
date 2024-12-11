ba604ca40594368b3e1d8326c7572dacd4eba702
Jorge Sanchez <xhercs@gmail.com>
2024-12-11 20:31:53 +0000

    Commited Program.cs with new dependency injection and dependencies to developertest project

--- Smartwyre.DeveloperTest.Runner/Program.cs
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Smartwyre.DeveloperTest.Factory;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Strategy;
+using Smartwyre.DeveloperTest.Types;
+using System;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
@@ -6,6 +11,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        throw new NotImplementedException();
+        //Registering the dependency injection
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<RebateCalculationFactory>()
+            .AddTransient<IRebateService, RebateService>()
+            .AddSingleton<IRebateCalculationStrategy, FixedCashAmountStrategy>()
+            .AddSingleton<IRebateCalculationStrategy, FixedRateRebateStrategy>()
+            .AddSingleton<IRebateCalculationStrategy, AmountPerUomStrategy>()
+            .BuildServiceProvider();
+
+        // Get the rebate service
+        var rebateService = serviceProvider.GetService<IRebateService>();
+
+        // Example
+        var request = new CalculateRebateRequest
+        {
+            RebateIdentifier = "REBATE123",
+            ProductIdentifier = "PRODUCT456",
+            Volume = 10
+        };
+
+        var result = rebateService.Calculate(request);
+        Console.WriteLine($"Calculation Success: {result.Success}");
     }
 }
