33e690a9139dc2141638a72e07f6aadfb704b553
Jorge Sanchez <xhercs@gmail.com>
2024-12-11 21:07:04 +0000

    Created facade pattern for rebate service

---
+++ Smartwyre.DeveloperTest/Facade/IRebateServiceFacade.cs
@@ -0,0 +1,12 @@
+﻿using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Facade;
+
+public interface IRebateServiceFacade
+{
+    Rebate GetRebate(string rebateIdentifier);
+    Product GetProduct(string productIdentifier);
+    void StoreCalculationResult(Rebate rebate, decimal rebateAmount);
+    CalculateRebateResult CreateResult(bool success);
+}
+
---
+++ Smartwyre.DeveloperTest/Facade/RebateServiceFacade.cs
@@ -0,0 +1,37 @@
+﻿using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Facade;
+
+public class RebateServiceFacade : IRebateServiceFacade
+{
+    private readonly RebateDataStore _rebateDataStore;
+    private readonly ProductDataStore _productDataStore;
+
+    public RebateServiceFacade()
+    {
+        _rebateDataStore = new RebateDataStore();
+        _productDataStore = new ProductDataStore();
+    }
+
+    public Rebate GetRebate(string rebateIdentifier)
+    {
+        return _rebateDataStore.GetRebate(rebateIdentifier);
+    }
+
+    public Product GetProduct(string productIdentifier)
+    {
+        return _productDataStore.GetProduct(productIdentifier);
+    }
+
+    public void StoreCalculationResult(Rebate rebate, decimal rebateAmount)
+    {
+        _rebateDataStore.StoreCalculationResult(rebate, rebateAmount);
+    }
+
+    public CalculateRebateResult CreateResult(bool success)
+    {
+        return new CalculateRebateResult { Success = success };
+    }
+}
+
